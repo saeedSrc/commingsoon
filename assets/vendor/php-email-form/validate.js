38943b79ac2120c704ad68b53e96fd8e785343c7
saeed <saeedrasooli421@gamil.com>
2021-02-19 13:13:48 +0330

    test

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -18,7 +18,7 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
+        displayError(thisForm, 'ورودی فرم پر نشده است')
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
